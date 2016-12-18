5e2294c8b17e38239900e971597ffdc5e21f9d42
yuly nurhidayati <yulynur2013@gmail.com>
2016-12-18 17:15:54 +0700

    mengapdate sample

--- app/src/main/java/pl/polak/clicknumberpicker/demo/MainActivity.java
+++ app/src/main/java/pl/polak/clicknumberpicker/demo/MainActivity.java
@@ -2,6 +2,14 @@ package pl.polak.clicknumberpicker.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import pl.polak.clicknumberpicker.ClickNumberPickerListener;
+import pl.polak.clicknumberpicker.ClickNumberPickerView;
+import pl.polak.clicknumberpicker.PickerClickType;
+
+import static android.widget.Toast.LENGTH_LONG;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ClickNumberPickerView picker = (ClickNumberPickerView) findViewById(R.id.my_picker_01);
+        final ClickNumberPickerView picker_02 = (ClickNumberPickerView) findViewById(R.id.my_picker_02);
+
+        picker.setClickNumberPickerListener(new ClickNumberPickerListener() {
+            @Override
+            public void onValueChange(float previousValue, float currentValue, PickerClickType pickerClickType) {
+
+                Log.d(this.getClass().getSimpleName(), "test");
+                Log.d(this.getClass().getSimpleName(), String.valueOf(picker.getValue()));
+
+                Toast.makeText(getApplication(),String.valueOf(picker.getValue()),LENGTH_LONG).show();
+            }
+        });
+
+        picker_02.setClickNumberPickerListener(new ClickNumberPickerListener() {
+            @Override
+            public void onValueChange(float previousValue, float currentValue, PickerClickType pickerClickType) {
+
+                Log.d(this.getClass().getSimpleName(), "test");
+                Log.d(this.getClass().getSimpleName(), String.valueOf(picker_02.getValue()));
+
+                Toast.makeText(getApplication(),String.valueOf(picker_02.getValue()),LENGTH_LONG).show();
+
+
+            }
+        });
+
     }
 }
